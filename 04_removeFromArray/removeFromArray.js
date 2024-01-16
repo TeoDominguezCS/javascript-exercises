9beddc340c27fc00c2c1e37188376f88df871f30
Teo <70834248+TeoDominguezCS@users.noreply.github.com>
2024-01-15 16:00:39 -0800

    complete js exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...remove) {
+    //iterate through all arguments passed
+    for(let i = 0; i < remove.length; i++){
+        //find indexof argument to be removed
+        let index = arr.indexOf(remove[i]);
+        //if it exists splice it remove 1 item AKA we're replacing it
+        //NOTE splice will modify array not create a new one
+        if(index >= 0 ){
+            arr.splice(index, 1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
