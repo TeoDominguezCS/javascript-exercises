3dd905c0b2ad942edfc795071d78353971dd50b4
Teo <70834248+TeoDominguezCS@users.noreply.github.com>
2024-01-16 13:04:05 -0800

    complete js exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,47 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return Number(num1 + num2);
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return Number(num1 - num2);
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let sum = 0;
+  for(let i = 0; i < arr.length; i++){
+    sum += arr[i];
+  }
+	return sum;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let sum = 1;
+  for(let i = 0; i < arr.length; i++){
+    sum *= arr[i];
+  }
+  return sum;
 
 };
 
-const power = function() {
-	
+const power = function(num, power) {
+  let result = num;
+  for(let i = 1; i < power; i++){
+    result *= num;
+  }
+	return result;
 };
 
-const factorial = function() {
+const factorial = function(num) {
+  if (num === 0){
+    return 1;
+  }
+  else{
+    result = num;
+    for(let i = 1; i < num; i++){
+      result *= num - i
+    }
+    return result;
+  }
 	
 };
 
