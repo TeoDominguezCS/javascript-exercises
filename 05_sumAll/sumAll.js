36f1d3e70f3159f633b2c6a67a043d873e387307
Teo <70834248+TeoDominguezCS@users.noreply.github.com>
2024-01-15 16:27:48 -0800

    optimize js exercise 05 solution

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,23 @@
-const sumAll = function() {
+const sumAll = function(min, max) {
+    let sum = 0;
+    //error checks
+    if(min < 0 || max < 0){
+        return "ERROR";
+    }
+    if(!Number.isInteger(max) || !Number.isInteger(min)){
+        return "ERROR";
+    }
+    //larger number placed first
+    if(min > max){
+        let temp = max;
+        max = min;
+        min = temp;
+    }
+    //finding the sum
+    for (let i = min; i <= max; i++){
+        sum += i;
+    }
+    return sum;
 
 };
 
